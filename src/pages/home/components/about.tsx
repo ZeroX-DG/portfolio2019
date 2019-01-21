a5c2e18c934bbdc42bf66b559a855a1deb359259
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2019-01-21 22:54:41 +0700

    updated about message

--- src/pages/home/components/about.tsx
+++ src/pages/home/components/about.tsx
@@ -27,6 +27,7 @@ class About extends React.Component {
   render() {
     return (
       <div className="about full-page" id="about">
+        <span className="sub-text">About me</span>
         <span className="name">Hung Nguyen</span>
         <div className="overflow-image">
           <img
@@ -48,9 +49,21 @@ class About extends React.Component {
           become a doctor, I ended up being a programmer, making magic happen
           with a keyboard.
           <h2 className="title">How did that happen?</h2>
-          At the age of 13, while every kid in my school obsessing with the new
-          game called League of Legends, I find myself spending all my time
-          learning about programming.
+          At the age of 13, while every kid in my school is obsessing with the
+          new game called League of Legends, I find myself spending all my time
+          learning about programming. Not that I don't like playing games, but
+          because my dad PC is not the gaming type. Therefore, writing
+          instructions for the computer seem to be the only fascinating thing I
+          can do at that time.
+          <br />
+          <br />
+          After years of studying programming, my dad decided that he will allow
+          me to become whoever I wanted, and that's when I dropped out of high
+          school to study for a higher diploma in software engineering at a
+          technology institute.
+          <h2 className="title">What's my current status?</h2>
+          Currently, I'm a studying abroad at SIT (Southern Institute of
+          Technology) in New Zealand.
         </div>
       </div>
     );
